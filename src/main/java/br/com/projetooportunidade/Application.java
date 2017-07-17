09bfcca3db6f6ec8023b5239df2aff2027039c5d
Luis <Luis@JUNIOR-NOTE>
2017-07-17 15:31:30 -0300

    Correção do package da classe Application

--- src/main/java/br/com/projetooportunidade/Application.java
+++ src/main/java/br/com/projetooportunidade/Application.java
@@ -1,14 +1,10 @@
-package br.com.projetooportunidade.util;
+package br.com.projetooportunidade;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
-@EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
